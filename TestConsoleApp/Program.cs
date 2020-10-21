c8169823d173641d2307a590c26796732f17df74
johnd <johndward01@yahoo.com>
2020-10-21 15:42:17 -0500

    made some changes

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -7,6 +7,15 @@ namespace TestConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("1st change");
+
+            Console.WriteLine("2nd change");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Made a change");
+            }
         }
     }
 }
